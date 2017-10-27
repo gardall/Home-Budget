cf48b85939cf7123feebb15c5b1ecb563643468e
Sebastian Lechowicz <s-lechowicz@outlook.com>
2017-10-27 02:29:55 +0200

    Login Works!

--- src/main/java/com/lechowiczfamily/homebudget/config/SpringSecurityConfig.java
+++ src/main/java/com/lechowiczfamily/homebudget/config/SpringSecurityConfig.java
@@ -1,11 +1,13 @@
 package com.lechowiczfamily.homebudget.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
+@Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Autowired
@@ -18,7 +20,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
                 .authorizeRequests()
                 .antMatchers("/", "/home", "/about").permitAll()
                 .antMatchers("/admin/**").hasAnyRole("ADMIN")
-                .antMatchers("/user/**").hasAnyRole("USER")
+                .antMatchers("/user/**").hasAnyRole("ADMIN","USER")
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
