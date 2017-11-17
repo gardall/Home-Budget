3aefdc5bacb73a9fdffe20b9c96645104883af8d
Sebastian Lechowicz <s-lechowicz@outlook.com>
2017-11-18 00:15:52 +0100

    Fix success registration redirection.

--- src/main/java/com/homebudget/controller/LoginController.java
+++ src/main/java/com/homebudget/controller/LoginController.java
@@ -50,7 +50,7 @@ public class LoginController {
             userService.saveUser(user);
             modelAndView.addObject("successMessage", "User has been registered successfully");
             modelAndView.addObject("user", new User());
-            modelAndView.setViewName("registration");
+            modelAndView.setViewName("login");
         }
         return modelAndView;
     }
