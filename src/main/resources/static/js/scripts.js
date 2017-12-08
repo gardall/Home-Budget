4aae79a7f0601a7aed1bacc5d64d2f3b666f6d2c
Sebastian Lechowicz <s-lechowicz@outlook.com>
2017-12-08 22:02:58 +0100

    Added temporary front side protection on target users selection.

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,4 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#allTransactionsTable').DataTable();
     $('#currentUserTransactionsTable').DataTable();
-} );
+    $('#submitTransactionFormButton').click(function () {
+        checked = $("input[type=checkbox]:checked").length;
+
+        if (!checked) {
+            alert("Check at least one target user!");
+            return false;
+        }
+    });
+});
